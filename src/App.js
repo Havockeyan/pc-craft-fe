b28c7200fbee7cc29b0f649c7e71671c046cf838
SAIRAMSSSS <hashiramasenju6.2006@gmail.com>
2025-07-09 17:36:48 +0530

    feat(hero.jsx):completed the landing page and edited css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import Hero from "./components/hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 function App() {
   return (
-    <div  className='bg-gray-900 min-h-screen'>
-    <Hero />
+    <div className="min-h-screen relative text-white overflow-x-hidden">
+    
+    <div className="absolute inset-0 bg-[radial-gradient(circle_at_right,_#6b21a8_30%,_#000000_100%)] z-[-2]" />
+
+   
+    <div className="absolute inset-0 animate-gradient-motion z-[-1] opacity-80 mix-blend-overlay" />
+
     <Navbar />
-    </div>
+    <Hero />
+  </div>
   );
 }
 
