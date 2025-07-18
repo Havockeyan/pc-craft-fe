43a545cc0056a398b589455faa1136c744921678
Havockeyan <karthihavoc121@gmail.com>
2025-07-18 15:03:22 +0530

    login and signup backend connection

---
+++ src/apiDomain.js
@@ -0,0 +1,5 @@
+// Utility to get the API domain for all requests
+export function getApiDomain() {
+  // You can change this value as needed for different environments
+  return 'http://localhost:3001';
+} 
